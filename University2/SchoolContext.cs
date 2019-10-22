6f7e7ec20ca2eb45d51940a5b2903082dd874d38
Josue Alva Zarate <josue.alva@tecsup.edu.pe>
2019-10-21 21:26:46 -0500

    first commit

---
+++ University2/SchoolContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using University2.Models;
+
+namespace University2
+{
+    public class SchoolContext : DbContext
+    {
+
+        public SchoolContext(): base ("name = MyContextDB"){ }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments  { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+
+        //Este con courses porque tiene relacion con courses
+        public DbSet<Instructor> Instructors { get; set; }
+
+        //Este no
+        public DbSet<Student> Students { get; set; }
+
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<Person> People { get; set; }
+
+    }
+}
